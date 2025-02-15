d317b1cdc74f15c540aac1d9be44a10c8fe8a955
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-02-15 18:43:48 +0300

    init

---
+++ test/player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+@onready var camera: Camera3D = $Camera3D
+
+const SPEED = 10
+const JUMP_VELOCITY = 7
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * 0.005)
+		camera.rotate_x(-event.relative.y * 0.005)
+		camera.rotation.x = clamp(camera.rotation.x,-PI/2,PI/2)
+			
+
+func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority(): return
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("a", "d", "w", "s")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
